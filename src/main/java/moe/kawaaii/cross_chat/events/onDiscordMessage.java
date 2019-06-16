bc150c83723440adb09648cc8a55ed4efe964bb0
天使アシュリー <ashley@kawaaii.moe>
2019-06-16 07:11:01 +0200

    🥰 I love when something works!

--- src/main/java/moe/kawaaii/cross_chat/events/onDiscordMessage.java
+++ src/main/java/moe/kawaaii/cross_chat/events/onDiscordMessage.java
@@ -1,12 +1,42 @@
 package moe.kawaaii.cross_chat.events;
 
+import org.apache.commons.lang.text.StrSubstitutor;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.javacord.api.entity.message.MessageAuthor;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static moe.kawaaii.cross_chat.mainClass.config;
+
 public class onDiscordMessage implements MessageCreateListener {
 
+    private String formatPlaceholders(MessageAuthor user, String message) {
+
+        String format = config.getString("config.message-formats.minecraft");
+        if (format == null) format = "[Discord] &l{username}&r: {message}";
+
+        Map<String, String> values = new HashMap<>();
+        values.put("username", user.getDisplayName());
+        values.put("user_id", user.getIdAsString());
+        values.put("message", message);
+
+        StrSubstitutor sub = new StrSubstitutor(values, "{", "}");
+
+        return sub.replace(ChatColor.translateAlternateColorCodes('&', format));
+    }
+
     @Override
-    public void onMessageCreate(MessageCreateEvent messageCreateEvent) {
+    public void onMessageCreate(MessageCreateEvent e) {
+
+        if (!e.getChannel().getIdAsString().equals(config.getString("config.discord.channels.discord-to-minecraft"))) return;
+        if (e.getMessage().getAuthor().isBotUser()) return;
+
+        String content = formatPlaceholders(e.getMessage().getAuthor(), e.getMessage().getReadableContent());
+        Bukkit.getServer().broadcastMessage(content);
 
     }
 }
